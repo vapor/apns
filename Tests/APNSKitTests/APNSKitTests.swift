fe062b7c9b33708509bec16fbdc3cdff8f91a5c4
Tanner Nelson <me@tanner.xyz>
2019-02-07 16:02:59 -0500

    fix test module names

--- Tests/APNSKitTests/APNSKitTests.swift
+++ Tests/APNSKitTests/APNSKitTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import apns_kit
+@testable import APSNKit
 
-final class apns_kitTests: XCTestCase {
+final class APNSKitTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct

2b6acdf1e332801e36309a70e8eae03c0bc7c687
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-27 05:21:19 -0600

    Match Vapor's minimum OS requirement and bump minimum required versions of dependencies. (#15)

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 let package = Package(
     name: "apns",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "APNS", targets: ["APNS"]),
     ],
     dependencies: [
         .package(url: "https://github.com/kylebrowning/APNSwift.git", from: "1.7.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.4"),
     ],
     targets: [
         .target(name: "APNS", dependencies: ["APNSwift", "Vapor"]),

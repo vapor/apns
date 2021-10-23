88ab7915d8e54b5f209095fad564e7473d323fbc
Tim <0xtimc@gmail.com>
2021-10-23 21:17:39 +0100

    Add remaining async APIs

--- Sources/APNS/APNS+Concurrency.swift
+++ Sources/APNS/APNS+Concurrency.swift
@@ -65,4 +65,31 @@ extension Application.APNS {
     }
 }
 
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+extension Request.APNS {
+    public func send(
+        rawBytes payload: ByteBuffer,
+        pushType: APNSwiftConnection.PushType,
+        to deviceToken: String,
+        expiration: Date?,
+        priority: Int?,
+        collapseIdentifier: String?,
+        topic: String?,
+        logger: Logger?,
+        apnsID: UUID? = nil
+    ) async throws {
+        try await send(rawBytes: payload, pushType: pushType, to: deviceToken, expiration: expiration, priority: priority, collapseIdentifier: collapseIdentifier, topic: topic, logger: logger, apnsID: apnsID).get()
+    }
+}
+
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+extension APNSConnectionSource {
+    public func makeConnection(
+        logger: Logger,
+        on eventLoop: EventLoop
+    ) async throws -> APNSwiftConnection {
+        try await APNSwiftConnection.connect(configuration: self.configuration, on: eventLoop).get()
+    }
+}
+
 #endif

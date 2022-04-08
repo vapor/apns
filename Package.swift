1a7adf0c8348a8bb5ab46ac6dfc8d44da86e298d
Simon Kempendorf <simon@code28.de>
2022-04-08 17:39:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "APNS", targets: ["APNS"]),
     ],
     dependencies: [
-        .package(name: "apnswift", url: "https://github.com/kylebrowning/APNSwift.git", from: "3.0.0"),
+        .package(name: "apnswift", url: "https://github.com/swift-server-community/APNSwift.git", "3.0.0"..."4.0.0"),
         .package(name: "vapor", url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
     ],

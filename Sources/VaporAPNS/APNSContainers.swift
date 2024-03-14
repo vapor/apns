1626c395df24c9f2f997b47e20651975c8ae68bd
Florian Friedrich <ffried@me.com>
2024-03-14 07:45:32 +0100

    Use NIOLockedValueBox

--- Sources/VaporAPNS/APNSContainers.swift
+++ Sources/VaporAPNS/APNSContainers.swift
@@ -6,7 +6,7 @@ import NIOConcurrencyHelpers
 
 public typealias APNSGenericClient = APNSClient<JSONDecoder, JSONEncoder>
 
-public class APNSContainers: @unchecked Sendable { // @unchecked, because the mutable state is protected by a lock.
+public final class APNSContainers: Sendable {
     public struct ID: Sendable, Hashable, Codable {
         public let string: String
         public init(string: String) {
@@ -18,30 +18,27 @@ public class APNSContainers: @unchecked Sendable { // @unchecked, because the mu
         public let configuration: APNSClientConfiguration
         public let client: APNSGenericClient
         
-         internal init(configuration: APNSClientConfiguration, client: APNSGenericClient) {
+        internal init(configuration: APNSClientConfiguration, client: APNSGenericClient) {
             self.configuration = configuration
             self.client = client
         }
     }
 
-    private var containers: [ID: Container]
-    private var defaultID: ID?
-    private var lock: NIOLock
+    private let storage: NIOLockedValueBox<(containers: [ID: Container], defaultID: ID?)>
 
     init() {
-        self.containers = [:]
-        self.lock = .init()
+        storage = .init((containers: [:], defaultID: nil))
     }
 
     public func syncShutdown() {
-        self.lock.lock()
-        defer { self.lock.unlock() }
-        do {
-            try containers.forEach { key, container in
-                try container.client.syncShutdown()
+        storage.withLockedValue {
+            do {
+                try $0.containers.values.forEach { container in
+                    try container.client.syncShutdown()
+                }
+            } catch {
+                fatalError("Could not shutdown APNS Containers")
             }
-        } catch {
-            fatalError("Could not shutdown APNS Containers")
         }
     }
 }
@@ -57,38 +54,37 @@ extension APNSContainers {
         as id: ID,
         isDefault: Bool? = nil
     ) {
-        self.lock.lock()
-        defer { self.lock.unlock() }
-
-        self.containers[id] = Container(
-            configuration: config,
-            client: APNSGenericClient(
+        storage.withLockedValue {
+            $0.containers[id] = Container(
                 configuration: config,
-                eventLoopGroupProvider: eventLoopGroupProvider,
-                responseDecoder: responseDecoder,
-                requestEncoder: requestEncoder,
-                byteBufferAllocator: byteBufferAllocator
+                client: APNSGenericClient(
+                    configuration: config,
+                    eventLoopGroupProvider: eventLoopGroupProvider,
+                    responseDecoder: responseDecoder,
+                    requestEncoder: requestEncoder,
+                    byteBufferAllocator: byteBufferAllocator
+                )
             )
-        )
 
-        if isDefault == true || (self.defaultID == nil && isDefault != false) {
-            self.defaultID = id
+            if isDefault == true || ($0.defaultID == nil && isDefault != false) {
+                $0.defaultID = id
+            }
         }
     }
 
     public func `default`(to id: ID) {
-        self.lock.lock()
-        defer { self.lock.unlock() }
-        self.defaultID = id
+        storage.withLockedValue {
+            $0.defaultID = id
+        }
     }
 
     public func container(for id: ID? = nil) -> APNSContainers.Container? {
-        self.lock.lock()
-        defer { self.lock.unlock() }
-        guard let id = id ?? self.defaultID else {
-            return nil
+        storage.withLockedValue {
+            guard let id = id ?? $0.defaultID else {
+                return nil
+            }
+            return $0.containers[id]
         }
-        return self.containers[id]
     }
 
     public var container: APNSContainers.Container? {
